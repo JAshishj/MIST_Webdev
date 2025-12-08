d58cee426c1ab3b3c137d675df9586019b5ca38b
Ashish Shenoy <ashishshenoy7@gmail.com>
2025-12-08 18:50:59 +0530

    Update index.js

--- Task-4/index.js
+++ Task-4/index.js
@@ -125,7 +125,7 @@ const movies = [
     genre: "Animation, Action, Adventure",
     rating: "8.4",
     description: "Teenager Miles Morales becomes Spider-Man and joins other Spider-People from across dimensions to stop a powerful threat.",
-    image: "Images/Into_the_spiderVerse.jpg"
+    image: "Images/Into_the_spiderverse.jpg"
   },
   {
     title: "Ford v Ferrari",
@@ -304,4 +304,5 @@ if (searchInput && searchResults && genreFilter) {
   searchInput.addEventListener("input", filterMovies);
   genreFilter.addEventListener("change", filterMovies);
 }
+
 
